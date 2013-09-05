92d7314a8db5bc76dc3a7e19fa4780db90e34087
Nina <ninamarie315@gmail.com>
2013-09-04 23:57:20 -0400

    finished the top half of the about page

---
+++ page-about.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: About
+*/
+?>
+
+<?php get_header('about'); ?>
+
+			<div id="content">
+			     <div id="about-content-top">
+                    Code Liberation was founded by Phoenix Perry and four women from various backgrounds who want to change the female-to-male ratio in video game development.
+			     </div>
+            </div> <!-- end #content -->
+
+<?php get_footer(); ?>
