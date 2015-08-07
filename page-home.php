037b8aed35b7b946227a8bf2ec4c74519403de54
Catt Small <cattsmall@gmail.com>
2015-08-07 11:05:24 -0400

    Update page-home.php

--- page-home.php
+++ page-home.php
@@ -23,57 +23,8 @@ Template Name: Home Page
 						<header class="article-header index-hero">
 							<h2><?php the_field('events_&_classes_header_text'); ?></h2>
 						</header> <!-- end article header -->
-						<?php
-						 $events = eo_get_events(array(
-						         'numberposts'=>3,
-						         'event_start_after'=>'today',
-						         'showpastevents'=>true,//Will be deprecated, but set it to true to play it safe.
-						    ));
-
-						  if($events):
-						     echo '<ul class="events-classes-list">';
-						     foreach ($events as $event):
-						          //Check if all day, set format accordingly
-						          $dateformat = ( get_option('date_format'));
-									 if( eo_reoccurs($event->ID) ){
-									 $recurring_format = 'l\s'.' @ '.get_option('time_format');
-								 	}else{
-  									 $recurring_format = get_option('time_format');
-								 	}
-								   $post_object = get_post($event->ID);
-									setup_postdata($post_object);
-								   $description = get_the_excerpt();
-									$event_category =  get_the_term_list( $event->ID, 'event-category', '', ', ','');
-									$btn_text = strip_tags($event_category);
-									$registration_link = get_post_meta($event->ID,'registration_link', true);
-									if ($registration_link) {
-										$reg_url = $registration_link;
-									}
-									else {
-										$reg_url = get_permalink($event->ID);
-									}
-						          printf(
-						             '<li>
-											<h3 class="event-title"><a href="%s">%s</a></h3>
-										 	<section class="event-info">
-											 	<span class="event-date">%s - %s</span>
-												<span class="event-time">%s</span>
-												<article class="event-desc">'.$description.'</article>
-												<a class="button" href="'.$reg_url.'">Join this '.$btn_text.'</a> 
-											</section>
-										 </li>',
-						             get_permalink($event->ID),
-						             get_the_title($event->ID),
-						             eo_get_the_start($dateformat, $event->ID,null,$event->occurrence_id),
-										 eo_get_the_end($dateformat, $event->ID,null,$event->occurrence_id),
-						             eo_get_the_start($recurring_format, $event->ID,null,$event->occurrence_id)
-						          );
-						     endforeach;
-						     echo '</ul>';
-						  endif;
-						 ?>
 						<footer>
-							<a href="<?php echo home_url(); ?>/events-classes"><?php the_field('events_&_classes_link'); ?></a> <!-- - <a href="#"><?php the_field('video_tutorials_link'); ?></a> -->
+							<a href="http://www.eventbrite.com/o/the-code-liberation-foundation-4653154663"><?php the_field('events_&_classes_link'); ?></a> <!-- - <a href="#"><?php the_field('video_tutorials_link'); ?></a> -->
 						</footer>
 					</section> <!-- end article section -->
 					
